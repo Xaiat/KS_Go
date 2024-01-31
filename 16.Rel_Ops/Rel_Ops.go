17d83ae69015311bdc466f105b86bb0ec86268aa
Xaiat <xaiat@xaiat.com>
2024-01-31 16:38:35 +0800

    Initial commit

--- 16.Rel_Ops/Rel_Ops.go
+++ 16.Rel_Ops/Rel_Ops.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var a int = 11
+	var b int = 10
+
+	// 关系运算符
+	// = 赋值
+	// == 等于
+	// != 不等于
+	// > 大于
+	// < 小于
+	// >= 大于等于
+	// <= 小于等于
+	fmt.Printf("a == b = %t\n", a == b) // 判断是否相等
+	fmt.Printf("a != b = %t\n", a != b) // 判断是否相等
+	fmt.Printf("a > b = %t\n", a > b)   // 判断是否相等
+	fmt.Printf("a < b = %t\n", a < b)   // 判断是否相等
+	fmt.Printf("a >= b = %t\n", a >= b) // 判断是否相等
+	fmt.Printf("a <= b = %t\n", a <= b) // 判断是否相等
+
+	// 判断 if 如果。。。那么。。。否则。。。
 }
