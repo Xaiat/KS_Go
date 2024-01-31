10abfe625d926e4f48c79810e2c2d1cdaa27abe6
Xaiat <xaiat@xaiat.com>
2024-01-31 17:42:19 +0800

    Initial commit

--- 17.Log_Ops/Log_Ops.go
+++ 17.Log_Ops/Log_Ops.go
@@ -5,9 +5,10 @@ import "fmt"
 func main() {
 
 	var a bool = true
-	var b bool = false
+	var b bool = true
 
-	// 逻辑运算符`&&`就是`与`, 我与你，就是我和你都要满足一个结果，才执行
+	// 逻辑运算符`&&`就是`与`, 我与你，就是我和你都要满足一个结果，才执行。(理解为：和、并且)
+	// a 和 b 都是 true 才会执行
 	if a == true && b == true {
 		fmt.Println(a && b)
 	}
