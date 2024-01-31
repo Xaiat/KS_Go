114b725bef5b35591ffb467b392e66cb97aaf9fc
Xaiat <xaiat@xaiat.com>
2024-01-31 17:20:20 +0800

    Initial commit

--- 17.Log_Ops/Log_Ops.go
+++ 17.Log_Ops/Log_Ops.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	var a bool = true
+	var b bool = false
+
+	// 逻辑运算符`&&`就是`与`, 我与你，就是我和你都要满足一个结果，才执行
+	if a == true && b == true {
+		fmt.Println(a && b)
+	}
+	fmt.Println(a && b)
 }
