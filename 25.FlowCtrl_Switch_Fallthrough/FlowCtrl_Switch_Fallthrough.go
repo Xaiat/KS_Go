4cdfcbe446908bad713e0c94270cd8da9d020207
Xaiat <xaiat@xaiat.com>
2024-02-01 16:05:36 +0800

    Initial Commit

--- 25.FlowCtrl_Switch_Fallthrough/FlowCtrl_Switch_Fallthrough.go
+++ 25.FlowCtrl_Switch_Fallthrough/FlowCtrl_Switch_Fallthrough.go
@@ -8,7 +8,7 @@ func main() {
 	switch a {
 	case false:
 		fmt.Println("1. case 条件为 false")
-		fallthrough // fallthrough 用来 case 穿透，不管下一个条件是否满足，都强制执行后面的 case
+		fallthrough // fallthrough 用来 case 穿透，不管下一个条件是否满足，都强制执行后面的 case。实际开发中很少使用。
 	case true:
 		if a == false {
 			break // break 终止，不管后面的条件是否满足，都不执行
