ef19d0c9f65db2c44cd9b5f09ba7df81afcc322b
Xaiat <xaiat@xaiat.com>
2024-02-02 22:47:47 +0800

    Initial Commit

--- 32.Func_Decl/Func_Decl.go
+++ 32.Func_Decl/Func_Decl.go
@@ -3,12 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	// 函数的调用
+	// 调用无参数，无返回值，的函数
 	printinfo()
 
+	// 调用有参数的函数，我们就需要按接收函数的参数类型来传递
 	myprint("Xaiat.com")
 
-	myprintnum(add(1, 2))
+	// 调用有返回值的函数，我们就需要按接收函数的返回值类型来接收
+	c := add(1, 2)
+	myprintnum(c)
+
+	// 调用有多个返回值的函数
+	x, y := swap("Xaiat", ".com")
+	fmt.Println(x, y)
 
 }
 
@@ -28,10 +35,13 @@ func myprintnum(num int) {
 }
 
 // 有2个参数，的函数
+// 有1个返回值，的函数
 func add(a int, b int) int {
 	c := a + b
 	return c
 }
 
-// 有1个返回值，的函数
 // 有多个返回值，的函数
+func swap(x, string, y string) (string, string) {
+	return y, x
+}
