2111fd475553851cb36b65c65ceb6e38a13e1576
Xaiat <xaiat@xaiat.com>
2024-02-02 22:37:17 +0800

    Initial Commit

--- 32.Func_Decl/Func_Decl.go
+++ 32.Func_Decl/Func_Decl.go
@@ -1,11 +1,37 @@
 package main
 
+import "fmt"
+
 func main() {
+	// 函数的调用
+	printinfo()
+
+	myprint("Xaiat.com")
+
+	myprintnum(add(1, 2))
 
 }
 
 // 无参数，无返回值，的函数
+func printinfo() {
+	fmt.Println("printinfo")
+}
+
 // 有1个参数，的函数
+func myprint(msg string) {
+	fmt.Println(msg)
+}
+
+// 有1个参数，的函数
+func myprintnum(num int) {
+	fmt.Println(num)
+}
+
 // 有2个参数，的函数
+func add(a int, b int) int {
+	c := a + b
+	return c
+}
+
 // 有1个返回值，的函数
 // 有多个返回值，的函数
