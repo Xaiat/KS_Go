815f222c61e36daa5e9a2bbb13826d78e0fb633b
Xaiat <xaiat@xaiat.com>
2024-02-05 22:17:44 +0800

    Initial Commit

---
+++ 38.Func_Recur/Func_Recur.go
@@ -0,0 +1,9 @@
+package main
+
+// 递归函数 Func_Recur (Function Recursive)
+// 定义：一个函数自己调用自己，就叫做递归函数
+// 注意：递归函数需要有一个出口，逐渐向出口靠近，没有出口就会形成死循环
+
+func main() {
+
+}
---
+++ 38.Func_Recur/go.mod
@@ -0,0 +1,3 @@
+module Func_Recur
+
+go 1.21.6
