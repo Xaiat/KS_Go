77d8a8b0dcf849c9a4806dc2f8444049373f7242
Xaiat <xaiat@xaiat.com>
2024-02-06 22:29:56 +0800

    Initial Commit

--- 39.Func_Defer/Func_Defer.go
+++ 39.Func_Defer/Func_Defer.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	f("1")
+	fmt.Println("2")
+	// 会被延迟到最后执行
+	defer f("3")
+	fmt.Println("4")
+	// 会被延迟到最后执行
+	defer f("5")
+	fmt.Println("6")
+	// 会被延迟到最后执行
+	defer f("7")
+	fmt.Println("8")
+	f("9")
+	f("10")
+}
+
+func f(s string) {
+	fmt.Println(s)
 }
