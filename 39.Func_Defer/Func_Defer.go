576adae1d09b9a8dba5c08833758ba73d4af029b
Xaiat <xaiat@xaiat.com>
2024-02-07 19:45:45 +0800

    Initial Commit

--- 39.Func_Defer/Func_Defer.go
+++ 39.Func_Defer/Func_Defer.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+/*
 func main() {
 	f("1")
 	fmt.Println("2")
@@ -20,4 +21,19 @@ func main() {
 
 func f(s string) {
 	fmt.Println(s)
+}
+*/
+
+// defer 语句可以用来做关闭操作：关闭文件，解锁资源，打印最终报告，关闭数据库连接等
+func main() {
+	a := 10
+	fmt.Println(a)
+	// 参数在这里就已经传递进去了，但是不会执行，会被延迟到最后执行
+	defer f(a)
+	a++
+	fmt.Println("end a = ", a)
+}
+
+func f(s int) {
+	fmt.Println("函数里面的 a = ", s)
 }
